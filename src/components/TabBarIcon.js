e20b434c7662d71e768139de9d435f770c4c0bad
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2022-08-25 22:06:58 -0500

    Se agrega tabs con íconos para navegación

---
+++ src/components/TabBarIcon.js
@@ -0,0 +1,36 @@
+import { View, StyleSheet, Image, Text } from 'react-native'
+import React from 'react'
+
+const TabBarIcon = ({ focused, source, text, width }) => {
+
+    return (
+        <View style={styles.view}>
+            <Image
+                source={source}
+                resizeMode="contain"
+                style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? "#26438f" : "#748c94",
+                }}
+            />
+            <Text
+                style={{
+                    color: focused ? "#26438f" : "#748c94",
+                    fontSize: 14,
+                    fontWeight: focused ? 'bold' : 'normal'
+                }}>
+                {text}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    view: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+
+export default TabBarIcon
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStackScreen from './HomeStackScreen';
+import ClientStackScreen from './ClientStackScreen';
+import TabBarIcon from '../components/TabBarIcon';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="HomeTabs" component={HomeStackScreen} />
-            {/* <Tab.Screen name="ClientTab" component={SettingsStackScreen} /> */}
+        <Tab.Navigator screenOptions={{ headerShown: false }}        >
+            <Tab.Screen
+                name="HomeTab"
+                component={HomeStackScreen}
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon
+                            focused={focused}
+                            source={require('../../assets/home.png')}
+                            text={'Principal'}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="ClientTab"
+                component={ClientStackScreen}
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon
+                            focused={focused}
+                            source={require('../../assets/clients.png')}
+                            text={'Clientes'}
+                        />
+                    )
+                }} />
         </Tab.Navigator>
     )
 }
